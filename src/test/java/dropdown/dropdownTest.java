10ddea12cfe0c05d0488f4f5d4719d7e9cef9256
aspatariu <spatariuandrei@yahoo.com>
2021-03-20 20:45:56 +0200

    1

--- src/test/java/dropdown/dropdownTest.java
+++ src/test/java/dropdown/dropdownTest.java
@@ -5,22 +5,24 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.DropdownPage;
 
+import java.util.List;
+
 public class dropdownTest extends BaseTests {
     @Test
      public void selectDromDropdownTest(){
-         var dropdownPage = homePage.clickDropDownLink();
+         DropdownPage dropdownPage = homePage.clickDropDownLink();
          String option= "Option 1";
              dropdownPage.clickFromDropDownList(option);
-         var selectedOptions = dropdownPage.getSelectedOptions();
+        List<String> selectedOptions = dropdownPage.getSelectedOptions();
         Assert.assertEquals(selectedOptions.size(),1,"incorrect number of selections");
         Assert.assertTrue(selectedOptions.contains(option),"Option not selected");
      }
     @Test
     public void selectOption2dropdownTest(){
-        var dropdownpage = homePage.clickDropDownLink();
+        DropdownPage dropdownpage = homePage.clickDropDownLink();
         String option2 = "Option 2";
         dropdownpage.clickFromDropDownList(option2);
-        var selectOption2 = dropdownpage.getSelectedOptions();
+        List<String> selectOption2 = dropdownpage.getSelectedOptions();
         Assert.assertEquals(selectOption2.size(),1,"Not the right size");
         Assert.assertTrue(selectOption2.contains(option2),"Option not selected");
 
